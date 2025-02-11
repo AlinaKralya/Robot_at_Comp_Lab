11c20223d1fe4a007a6372a3da7892594e475a99
AlinaKralya <alinakralya04@gmail.com>
2025-02-10 19:01:24 -0800

    Updated the header with all of the pins and functions

--- Robot_ARC.ino
+++ Robot_ARC.ino
@@ -1,31 +1,59 @@
-/*-----------------------------------------------------------------------------------------
-Robot Code ARC Spring 2025
-------------------------------------------------------------------------------------------*/
+//-----------------------------------------------------------------------------------------
+//Robot Code ARC Spring 2025
+//-----------------------------------------------------------------------------------------
 #include <Servo.h>
 
-Servo Lpan;  
+//Initializing all of the Servos
+//------------------------------
+//Left side
+Servo Lpan;
 Servo Ltilt;
 Servo Lelbow;
+
+//Right side
 Servo Rpan;
 Servo Rtilt;
 Servo Relbow;
+//------------------------------
 
-int LPval = 176, LTval = 35, LEval = 180, x = 0, RPval = 13, RTval = 117, REval = 2; // Start positions
+//Pins for the servos
+const int Lpan_pin = 2;
+const int Ltilt_pin = 3;
+const int Lelbow_pin = 4;
+
+const int Rpan_pin = 5;
+const int Rtilt_pin = 6;
+const int Relbow_pin = 7;
+
+//Initial positions
+int LPval = 176, LTval = 35, LEval = 180;
+int RPval = 13, RTval = 117, REval = 2;
+
+//Functions
+void wave();
+
+
+//--------------------------------------MAIN-BODY------------------------------------------
 
 void setup() {
-  // put your setup code here, to run once:
-  Lpan.attach(2);    // attach the servos
-  Ltilt.attach(3);
-  Lelbow.attach(4);
-  Rpan.attach(5);
-  Rtilt.attach(6);
-  Relbow.attach(7);
-  Lpan.write(LPval); // Set start positions
+  //Attach servos to the Board 1
+  Lpan.attach(Lpan_pin);
+  Ltilt.attach(Ltilt_pin);
+  Lelbow.attach(Lelbow_pin);
+
+  Rpan.attach(Rpan_pin);
+  Rtilt.attach(Rtilt_pin);
+  Relbow.attach(Relbow_pin);
+
+  //Set servos to their starting positions
+  Lpan.write(LPval);
   Ltilt.write(LTval);
   Lelbow.write(LEval);
+  
   Rpan.write(RPval);
   Rtilt.write(RTval);
   Relbow.write(REval);
+  
 }
 
 void loop() {
